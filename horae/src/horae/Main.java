18d1a3b3ca3e2074ee5066eebe141f32b5895713
Fernando Gines <fgines@gmail.com>
2007-11-02 12:29:41 +0000

    Adicionando o projeto do compilador
    git-svn-id: http://svn.firstp.apanela.com/ffcc@3 b63d3ce4-a13d-0410-9108-9ce48ca835ef

---
+++ horae/src/horae/Main.java
@@ -0,0 +1,49 @@
+/*
+ * Main.java
+ *
+ * Created on November 2, 2007, 11:20 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package horae;
+
+import java.util.logging.ConsoleHandler;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        info();
+    }
+    
+    public static void info() {
+        System.out.println("As horae (vulgarmente designadas como horas pela" +
+                " corrupção do vocábulo original), constituíam um grupo de" +
+                " deusas gregas que presidiam às estações dos anos. Eram " +
+                " filhas de Zeus e Têmis são: Irene (paz), Dike (justiça) e " +
+                " Eumônia (disciplina); estas são as Horas mais velhas e estão" +
+                " ligadas a legislação e ordem natural, sendo uma extensão dos" +
+                " atributos de sua mãe Têmis. Eumônia está relacionada com a" +
+                " representação da divindade da justiça. Temis e Dike elucidam" +
+                " o lado ético do instinto, a voz miúda e calma no seio do" +
+                " impulso. Dike para a humanidade é a função de base institual" +
+                " muito sintônica com o que chama de instinto para reflexão. \n" +
+                "Existem mais nove Horas que são guardiãs da ordem natural, do" +
+                " ciclo anual de crescimento da vegetação e das estações" +
+                " climaticas anuais. (Talo, Carpo, Auxo, Acme, Anatole, Disis," +
+                " Dicéia, Eupória, Gimnásia).");
+    }
+}
